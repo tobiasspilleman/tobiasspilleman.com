d467459f5e3f90747fedaa116d17ce0e6029fad3
Tobias Spilleman <110896400+tobiasspilleman@users.noreply.github.com>
2024-10-01 12:30:05 +0200

    🔥 Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import React from 'react';
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <></>
 );
 
 export default IndexPage;

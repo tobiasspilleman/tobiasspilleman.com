d282057127329df7e62981ad7f3ef53e3a1a817e
Tobias Spilleman <110896400+tobiasspilleman@users.noreply.github.com>
2024-11-15 16:33:53 +0100

    🔥 Removing AnimatedGallery Code

--- components/AnimatedGalleryComponent.tsx
+++ components/AnimatedGalleryComponent.tsx
@@ -1,34 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-const AnimatedGalleryComponent = ({ images }: { images: string[] }) => {
-    const [slideIndex, setSlideIndex] = useState(0);
+const AnimatedGalleryComponent = ({ images, direction, speed, imgHeight, imgWidth }: { images: string[], direction: 'horizontal' | 'vertical', speed: number, imgHeight: number, imgWidth: number }) => {
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setSlideIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 2000);
 
-        return () => clearInterval(interval);
-    }, [images]);
-
-    return (
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {images.map((image, index) => (
-                <img
-                    key={index}
-                    src={image}
-                    alt=""
-                    style={{
-                        transform: `translateX(${(index - slideIndex) * 100}%)`,
-                        transition: 'transform 0.5s',
-                        width: '100px',
-                        height: '100px',
-                        objectFit: 'cover',
-                    }}
-                />
-            ))}
-        </div>
-    );
 };
 
 export default AnimatedGalleryComponent;

705b528065eb3d91337028958254c9e96ca509ba
Tobias Spilleman <110896400+tobiasspilleman@users.noreply.github.com>
2024-11-15 17:32:08 +0100

    💄 Implement empty projects into homepage

--- components/ProjectPreviewComponent.tsx
+++ components/ProjectPreviewComponent.tsx
@@ -1,9 +1,26 @@
-const ProjectPreviewComponent = () => {
-    return (
-        <div className="project-preview">
+import { Project } from "../interfaces/Project";
 
-        </div>
-    );
+/**
+ * Component to preview a project.
+ *
+ * @param {Object} props - The component props.
+ * @param {Project} props.project - The project to preview.
+ * @returns {JSX.Element} The rendered component.
+ */
+const ProjectPreviewComponent = ({ project }: { project?: Project }) => {
+    if (!project) {
+        return (
+            <div className="border-4 border-dashed border-gray-300 rounded min-h-[50px] min-w-[20px]">
+            </div>
+        );
+    } else {
+        return (
+            <div className="border-4 border-dashed border-gray-300 rounded min-h-[50px] min-w-[20px]">
+                <h3 className="text-center text-2xl font-bold">{project.name}</h3>
+                <p className="text-center text-lg">{project.description}</p>
+            </div>
+        );
+    }
 }
 
 export default ProjectPreviewComponent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,44 @@ import AboutComponent from '../components/AboutComponent';
 import WelcomeComponent from '../components/WelcomeComponent';
 import NavigationBar from '../components/NavigationBar';
 import ProjectPreviewComponent from "../components/ProjectPreviewComponent";
+import Button from "../components/button";
 
 const IndexPage = () => (
   <>
     <div className="App">
       <NavigationBar />
     </div>
-    <section className="home">
-      <WelcomeComponent />
-      <AboutComponent />
-        <ProjectPreviewComponent />
-    </section>
+      <section className="home">
+          <WelcomeComponent/>
+          <AboutComponent/>
+          <h2 className="text-center text-5xl font-bold my-10">Projects</h2>
+
+          <div className="flex flex-row flex-wrap justify-between mx-[15%] gap-5 mb-5">
+              <div className="flex-1">
+                  <ProjectPreviewComponent/>
+              </div>
+              <div className="flex-1">
+                  <ProjectPreviewComponent/>
+              </div>
+              <div className="flex-1">
+                  <ProjectPreviewComponent/>
+              </div>
+          </div>
+          <div className="flex flex-row flex-wrap justify-between mx-[15%] gap-5">
+              <div className="flex-1">
+                  <ProjectPreviewComponent/>
+              </div>
+              <div className="flex-1">
+                  <ProjectPreviewComponent/>
+              </div>
+              <div className="flex-1">
+                  <ProjectPreviewComponent/>
+              </div>
+          </div>
+          <div className="mx-[15%] my-5">
+              <Button text="All projects" callBackFunction={console.log}/>
+            </div>
+      </section>
   </>
 );
 

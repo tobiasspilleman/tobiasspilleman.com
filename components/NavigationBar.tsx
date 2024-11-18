4652a05a2d1a8e9b6d665f9d891ff0a95d28b792
Tobias Spilleman <110896400+tobiasspilleman@users.noreply.github.com>
2024-11-18 11:30:32 +0100

    💄 Improving navbar styling

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -6,7 +6,7 @@ const NavigationBar = () => {
 
     return (
         <>
-            <nav className="full bg-transparent px-10 lg:px- py-4 border-b border-gray-300 flex items-center justify-center sticky top-0">
+            <nav className="full bg-white px-[5vw] lg:px- py-4 border-b border-gray-300 flex items-center justify-center sticky top-0">
                 <div>
                     <a href='https://tobiasspilleman.com' className="flex items-center">
                         <Image src={logo} alt="Logo" className="w-7 h-7 mr-3" />
@@ -14,11 +14,9 @@ const NavigationBar = () => {
                     </a>
                 </div>
                 <div className="flex justify-end ml-auto">
-                    <a href="#" className="text-sm font-inter text-gray-600 font-medium">Home</a>
-                    <a href="/blog" className="text-sm font-inter text-gray-600 font-medium ml-4">Blog</a>
+                    <a href="/" className="text-sm font-inter text-gray-600 font-medium">Home</a>
                     <a href="/projects" className="text-sm font-inter text-gray-600 font-medium ml-4">Projects</a>
-                    <a href="/about" className="text-sm font-inter text-gray-600 font-medium ml-4">About</a>
-                    <a href="#" className="text-sm font-inter text-gray-600 font-medium ml-4">Contact</a>
+                    <a href="mailto:hello@tobiasspilleman.com" className="text-sm font-inter text-gray-600 font-medium ml-4">Contact</a>
                 </div>
             </nav>
         </>

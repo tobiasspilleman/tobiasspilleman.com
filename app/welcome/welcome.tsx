542da4e34b45e835409121585b2bce44bed15ef8
Tobias Spilleman <t.spilleman@gmail.com>
2025-12-09 22:58:40 +0100

    removing old welcome.tsx

--- app/welcome/welcome.tsx
+++
@@ -1,7 +0,0 @@
-export function Welcome() {
-  return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <h1 className="text-3xl font-bold">Welcome to React Router!</h1>
-    </main>
-  );
-}
